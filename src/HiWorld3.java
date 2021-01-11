48875d11555f224825d7084ac97700ce6bd25da0
jq7888 <jq.7888@qq.com>
2021-01-11 16:37:37 +0800

    first commit

---
+++ src/HiWorld3.java
@@ -0,0 +1,16 @@
+/**
+ * ClassName:HiWorld3
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Date:2020/12/30 22:49
+ * @Author:jiaqi@163.com
+ */
+public class HiWorld3 {
+    public static void main(String[] args) {
+        System.out.println("Hi World 3  h aha  !");
+        System.out.println("我是dev分支");
+        System.out.println("往github提交");
+        System.out.println("");
+    }
+}
---
+++ src/a.java
@@ -0,0 +1,11 @@
+/**
+ * ClassName:a
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Date:2021/1/6 12:30
+ * @Author:jiaqi@163.com
+ */
+public class a {
+
+}
